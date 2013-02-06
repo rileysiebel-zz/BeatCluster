df20537acfc73c11aa99c9f83c7fbe4547ec6049
Riley <riley.siebel@gmail.com>
2013-02-05 21:42:06 -0600

    Media player controls should work

--- BeatCluster/BCHomeViewController.h
+++ BeatCluster/BCHomeViewController.h
@@ -7,16 +7,21 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MediaPlayer/MediaPlayer.h>
 
-@interface BCHomeViewController : UIViewController
+@interface BCHomeViewController : UIViewController <MPMediaPickerControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *albumImageView;
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UILabel *artistLabel;
 @property (weak, nonatomic) IBOutlet UILabel *albumLabel;
 @property (weak, nonatomic) IBOutlet UISlider *volumeSlider;
+@property (weak, nonatomic) IBOutlet UIButton *playPauseButton;
+@property (weak, nonatomic) MPMusicPlayerController *musicPlayer;
 
 - (IBAction)previousSong:(id)sender;
 - (IBAction)playPause:(id)sender;
 - (IBAction)nextSong:(id)sender;
 - (IBAction)volumeChanged:(id)sender;
+- (IBAction)showMediaPicker:(id)sender;
+- (void)registerMediaPlayerNotifications;
 @end
